2e69783fa8ae2406d78b6995bba7013d972d75b5
DESKTOP-CM4DN2T\joncatlin <jonc@destini.com>
2018-11-16 18:21:42 -0700

    Changed the message to see test the deploy again

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 echo "****************************************************************"
-echo "hello jon"
+echo "hello everyone"
 echo "This is proof that the container was built and runs correctly"
 echo "****************************************************************"
